64a5e50ee69183e58dac199dc4a5a81c5b0d46da
JoshEndless <jharris@endlessstudios.com>
2023-05-01 16:31:56 -0500

    Sound Manager Stub

---
+++ Assets/Scripts/Sound/SoundEmitter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Sound
+{
+    public class SoundEmitter : MonoBehaviour
+    {
+        [SerializeField] private AudioClip clip;
+        [SerializeField] private float audioClipDB;
+        [SerializeField] private SoundManager.SoundEnum soundKind;
+
+        public void EmitSound()
+        {
+            SoundManager.EmittedSoundData soundData = new(transform.position, audioClipDB, soundKind, clip.length);
+            SoundManager.Instance.EmitSoundAtPosition(soundData, clip);
+        }
+    }
+}
---
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+namespace Sound
+{
+    public class SoundManager : MonoBehaviourSingleton<SoundManager>
+    {
+        public static event Action<EmittedSoundData> OnSoundEmitted;
+
+        private static void SoundEmitted(EmittedSoundData soundData) => OnSoundEmitted?.Invoke(soundData);
+
+        public void EmitSoundAtPosition(EmittedSoundData soundData, AudioClip clip)
+        {
+            //PlayClip from pooled audiosource
+            SoundEmitted(soundData);
+        }
+
+        public readonly struct EmittedSoundData
+        {
+            public readonly Vector3 Position;
+            public readonly float DBAtSource;
+            public readonly SoundEnum SoundKind;
+            public readonly float ClipLength;
+
+            public EmittedSoundData(Vector3 position, float dbAtSource, SoundEnum soundKind, float clipLength)
+            {
+                Position = position;
+                DBAtSource = dbAtSource;
+                SoundKind = soundKind;
+                ClipLength = clipLength;
+            }
+        }
+
+        public enum SoundEnum
+        {
+            //These could be more granular if the behaviour doesn't match expectations
+            AiGenerated,
+            Prop,
+            PlayerGenerated
+        }
+    }
+}
