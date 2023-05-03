a106d22e9fdd62dbfcf9622772afb35110a217ad
Alex Bascom <abascom@endlessstudios.com>
2023-05-03 09:03:30 -0700

    Made grabbable abstract

--- Assets/Scripts/InteractablesSystem/Grabbable.cs
+++ Assets/Scripts/InteractablesSystem/Grabbable.cs
@@ -2,12 +2,19 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class Grabbable : Interactable
+public abstract class Grabbable : Interactable
 {
+    [SerializeField] UnityEvent OnInteratStopped;
+
     protected override string DefaultInterationText => "Grab";
 
     internal void StopInteract()
     {
+        HandleStopInteract();
+        OnInteratStopped.Invoke();
     }
+
+    protected abstract void HandleStopInteract();
 }
