3074c7311e3e1b86079cd6490b47247c195104a9
olopezendless <olopez@endlessstudios.com>
2023-05-08 15:51:57 -0700

    Slidable Events, Code cleanup/comments,

--- Assets/Scripts/InteractablesSystem/Slidable.cs
+++ Assets/Scripts/InteractablesSystem/Slidable.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Events;
 
 [RequireComponent(typeof(Rigidbody))]
 public class Slidable : Grabbable
@@ -10,53 +10,69 @@ public class Slidable : Grabbable
 
     [SerializeField] private SliderContext sliderContext;
     [SerializeField] private float maxSlideSpeed = 5f;
+    [SerializeField, Min(.05f)] private float smoothSnapTime = .1f;
 
+    [SerializeField] private UnityEvent<Vector2> OnPositionMoved = new UnityEvent<Vector2>(); //Move finished event, Vector2 result in context's local space
+    [SerializeField] private UnityEvent<Vector2Int> OnPositionSnapped = new UnityEvent<Vector2Int>(); //Snap finished, Vector2Int x,y snap index
 
     private Vector3 targetPosition;
     private bool interactionActive;
     private Rigidbody rigidbody;
-    [SerializeField] private float multi = 5f;
+    private Coroutine activeSnapCoroutine;
 
     void Awake()
     {
+        if (sliderContext == null)
+        {
+            Debug.LogWarning("Slider Context missing. Removing Slidable.");
+            GameObject.Destroy((Slidable)this);
+            return;
+        }
+
+        //setup the rigidbody to slide properly
         rigidbody = GetComponent<Rigidbody>();
         rigidbody.isKinematic = true;
         rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
         rigidbody.useGravity = false;
         rigidbody.interpolation = RigidbodyInterpolation.Extrapolate;
         rigidbody.mass = 0;
+        rigidbody.drag = 0;
     }
-
-    void Update() => transform.localRotation = Quaternion.identity;
 
     protected override void InternalHandleInteract()
     {
         base.InternalHandleInteract();
-        interactionActive = true;
-        rigidbody.isKinematic = false;
-        targetPosition = rigidbody.position;
 
-        Debug.Log("enter");
+        if (activeSnapCoroutine != null) //cancel snapping
+        {
+            StopCoroutine(activeSnapCoroutine);
+            activeSnapCoroutine = null;
+        }
+
+        interactionActive = true;
+        rigidbody.isKinematic = false; //allow movement from the rigidbody's velocity
+        targetPosition = rigidbody.position; //init target pos
     }
 
     public override void HandleUpdate()
     {
+        //Plane based on the slidercontext's orientation
         Plane dragPlane = new Plane(sliderContext.transform.forward, sliderContext.transform.position);
-        Camera cam = Camera.main;//Camera.main.transform.GetComponentsInChildren<Camera>()[1];
+        Camera cam = Camera.main;
         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
 
+        //Raycast against the plane to get the desired drag position
         if (dragPlane.Raycast(ray, out float hitDistance))
         {
             Vector3 hitPoint = ray.GetPoint(hitDistance);
-            targetPosition = sliderContext.ClampPosition(hitPoint);
+            targetPosition = sliderContext.ClampPosition(hitPoint); //Clamp the desired drag position to the context's bounds
         }
         else
         {
             targetPosition = rigidbody.position;
         }
 
-        Debug.DrawLine(Camera.main.transform.position, targetPosition, Color.red);
-        Debug.DrawLine(rigidbody.position, rigidbody.position + (rigidbody.velocity * 4f), Color.yellow);
+        Debug.DrawLine(cam.transform.position, targetPosition, Color.red);
     }
 
     protected override void HandleStopInteract()
@@ -65,9 +81,30 @@ public class Slidable : Grabbable
         rigidbody.isKinematic = true;
         rigidbody.velocity = Vector3.zero;
 
-        rigidbody.MovePosition(sliderContext.SnapPosition(rigidbody.position));
+        activeSnapCoroutine = StartCoroutine(SmoothToPosition(sliderContext.EndSlide(transform.position))); //smooth to final position based on snap result
+    }
 
-        Debug.Log("exit");
+    IEnumerator SmoothToPosition(SliderContext.EndSlideResult snapResult)
+    {
+        Vector3 startPosition = transform.position;
+        float elapsedTime = 0;
+
+        while (elapsedTime < smoothSnapTime)
+        {
+            elapsedTime += Time.deltaTime;
+            float lerpT = elapsedTime / smoothSnapTime;
+            transform.position = (Vector3.Lerp(startPosition, snapResult.worldPosition, lerpT));
+            yield return null;
+        }
+
+        transform.position = snapResult.worldPosition;
+
+        if (snapResult.snap)
+            OnPositionSnapped.Invoke(snapResult.snapIndex);
+
+        OnPositionMoved.Invoke(snapResult.localPosition);
+        // Debug.Log($"Snap: { snapResult.snapIndex } , Pos: { snapResult.localPosition }");
+        activeSnapCoroutine = null;
     }
 
     void FixedUpdate()
@@ -75,21 +112,17 @@ public class Slidable : Grabbable
         if (interactionActive)
         {
             Vector3 moveDirection = targetPosition - rigidbody.position;
-            Debug.DrawLine(rigidbody.position, rigidbody.position + moveDirection, Color.green);   
             Vector3 moveVelocity = moveDirection.normalized * maxSlideSpeed * Time.fixedDeltaTime;
 
-            if((moveVelocity.magnitude * Time.fixedDeltaTime) > moveDirection.magnitude)
+            if ((moveVelocity.magnitude * Time.fixedDeltaTime) > moveDirection.magnitude) //check to see if the drag will overshoot the target position
             {
                 rigidbody.velocity = Vector3.zero;
                 return;
-            }                
-            
-            rigidbody.velocity = moveVelocity;      
-        }
-        else
-        {
-            rigidbody.MovePosition(sliderContext.SnapPosition(rigidbody.position));
+            }
+
+            rigidbody.velocity = moveVelocity; //move the slidable respecting physics
+
+            Debug.DrawLine(rigidbody.position, rigidbody.position + moveDirection, Color.green);
         }
     }
-
 }
--- Assets/Scripts/InteractablesSystem/SliderContext.cs
+++ Assets/Scripts/InteractablesSystem/SliderContext.cs
@@ -11,63 +11,112 @@ public class SliderContext : MonoBehaviour
     public bool SnapEnabled => snapEnabled;
     private EdgeCollider2D runtimeEdgeCollider;
 
+    public class EndSlideResult
+    {
+        public bool snap;
+        public Vector2Int snapIndex;
+        public Vector3 localPosition;
+        public Vector3 worldPosition;
+    }
+
+    internal class SnapAxisResult
+    {
+        public int snapIndex;
+        public float snapPosition;
+    }
+
     private void Awake()
     {
         SnapAllSlidables();
     }
 
-    public Vector3 ClampPosition(Vector3 position)
-    {
-        Vector3 relativePosition = transform.InverseTransformPoint(position);
-        relativePosition.x = Mathf.Clamp(relativePosition.x, -bounds.x, bounds.x);
-        relativePosition.y = Mathf.Clamp(relativePosition.y, -bounds.y, bounds.y);
-        // relativePosition.z = 0;
-        return transform.TransformPoint(relativePosition);
-    }
-
-    public Vector3 SnapPosition(Vector3 position)
-    {
-        Vector3 relativePosition = transform.InverseTransformPoint(position);
-        Vector3 snapOffset = relativePosition;
-
-        if (snapEnabled)
-        {
-            if (snapPoints.x > 0)
-                snapOffset.x = RoundToNearestSnapPoint(relativePosition.x, bounds.x, snapPoints.x);
-
-            if (snapPoints.y > 0)
-                snapOffset.y = RoundToNearestSnapPoint(relativePosition.y, bounds.y, snapPoints.y);
-        }
-
-        snapOffset.z = 0;
-
-        return transform.TransformPoint(snapOffset);
-    }
-
-    internal static float RoundToNearestSnapPoint(float value, float bounds, int pointCount)
-    {
-        if (pointCount == 1)
-            return 0;
-
-        float snapSize = 2 * bounds / (pointCount - 1); // Calculate the size of each snap point
-        int snapIndex = Mathf.RoundToInt((value + bounds) / snapSize); // Calculate the index of the nearest snap point
-        // Debug.Log("SnapIndex: " + snapIndex);
-        float snappedValue = snapIndex * snapSize - bounds; // Calculate the value of the nearest snap point
-        return snappedValue;
-    }
-
-    [ContextMenu("Snap All Children")] // Can be called from context menu in editor
+    [ContextMenu("Snap All Children")] // Can be called from context menu in editor to fix position in scene
     private void SnapAllSlidables()
     {
         Slidable[] allslidables = gameObject.GetComponentsInChildren<Slidable>();
 
         foreach (Slidable slidable in allslidables)
         {
-            slidable.transform.position = SnapPosition(slidable.transform.position);
+            slidable.transform.position = EndSlide(slidable.transform.position).worldPosition;
         }
     }
 
-    void OnDrawGizmos()//Selected
+    /// <summary>
+    /// Clamps the given position vector to be within the bounds of the SliderContext.
+    /// </summary>
+    /// <param name="position">The unclamped position.</param>
+    /// <returns>The clamped position on the SliderContext's plane.</returns>
+    public Vector3 ClampPosition(Vector3 position)
+    {
+        Vector3 relativePosition = transform.InverseTransformPoint(position);// Transform the position into the object's local space
+
+        // Clamp the position within bounds
+        relativePosition.x = Mathf.Clamp(relativePosition.x, -bounds.x, bounds.x);
+        relativePosition.y = Mathf.Clamp(relativePosition.y, -bounds.y, bounds.y);
+        relativePosition.z = 0;
+
+        return transform.TransformPoint(relativePosition);// Result in World Space
+    }
+
+    /// <summary>
+    /// Processes a slidables end position and provides resulting information after snap & local plane corrections. 
+    /// </summary>
+    /// <param name="position">The current position of the Slidable.</param>
+    /// <returns>EndSlideResult with position & snap info.</returns>
+    public EndSlideResult EndSlide(Vector3 position)
+    {
+        EndSlideResult result = new EndSlideResult();
+        result.snap = snapEnabled;
+        result.localPosition = transform.InverseTransformPoint(position);
+
+        if (snapEnabled)
+        {
+            //get snap results for x and y axis
+            if (snapPoints.x > 0)
+            {
+                SnapAxisResult xSnapResult = RoundToNearestSnapPoint(result.localPosition.x, bounds.x, snapPoints.x);
+                result.snapIndex.x = xSnapResult.snapIndex;
+                result.localPosition.x = xSnapResult.snapPosition;
+            }
+
+            if (snapPoints.y > 0)
+            {
+                SnapAxisResult ySnapResult = RoundToNearestSnapPoint(result.localPosition.y, bounds.y, snapPoints.y);
+                result.snapIndex.y = ySnapResult.snapIndex;
+                result.localPosition.y = ySnapResult.snapPosition;
+            }
+        }
+
+        result.localPosition.z = 0;
+        result.worldPosition = transform.TransformPoint(result.localPosition);
+        return result;
+    }
+
+    /// <summary>
+    /// Get the snap result of a specific axis.
+    /// </summary>
+    /// <param name="value">The current relative position on the corresponding axis.</param>
+    /// <param name="bounds">The contexts bounds of the corresponding axis.</param>
+    /// <param name="pointCount">The contexts snap point count of the corresponding axis.</param>
+    /// <returns>SnapAxisResult with  resulting local position & snap index.</returns>
+    internal static SnapAxisResult RoundToNearestSnapPoint(float value, float bounds, int pointCount)
+    {
+        SnapAxisResult result = new SnapAxisResult();
+
+        if (pointCount == 1)
+        {
+            result.snapIndex = 0;
+            result.snapPosition = 0; //snap to center
+            return result;
+        }
+
+        float snapDelta = 2 * bounds / (pointCount - 1); // Calculate the size of each snap point
+        result.snapIndex = Mathf.RoundToInt((value + bounds) / snapDelta); // Calculate the index of the nearest snap point      
+        result.snapPosition = result.snapIndex * snapDelta - bounds; // Calculate the position of of the nearest snap point
+        return result;
+    }
+
+    void OnDrawGizmos()
     {
         Vector3 corner1 = transform.TransformPoint(new Vector3(-bounds.x, bounds.y));
         Vector3 corner2 = transform.TransformPoint(new Vector3(bounds.x, bounds.y));
@@ -79,19 +128,19 @@ public class SliderContext : MonoBehaviour
         Gizmos.DrawLine(corner2, corner3);
         Gizmos.DrawLine(corner3, corner4);
         Gizmos.DrawLine(corner4, corner1);
-        Gizmos.color = Color.red;
 
         if (snapEnabled)
         {
-            int virtualSnapPointsX = snapPoints.x > 1 ? snapPoints.x - 1 : 0;
-            int virtualSnapPointsY = snapPoints.y > 1 ? snapPoints.y - 1 : 0;
+            Gizmos.color = Color.red;
+            int virtualSnapPointsX = snapPoints.x > 1 ? snapPoints.x - 1 : 1;
+            int virtualSnapPointsY = snapPoints.y > 1 ? snapPoints.y - 1 : 1;
 
             for (int x = 0; x < virtualSnapPointsX + 1; x++)
             {
                 for (int y = 0; y < virtualSnapPointsY + 1; y++)
                 {
-                    float xOffset = (x * ((bounds.x * 2) / virtualSnapPointsX)) - bounds.x;
-                    float yOffset = (y * ((bounds.y * 2) / virtualSnapPointsY)) - bounds.y;
+                    float xOffset = virtualSnapPointsX > 1 ? (x * ((bounds.x * 2) / virtualSnapPointsX)) - bounds.x : 0;
+                    float yOffset = virtualSnapPointsY > 1 ? (y * ((bounds.y * 2) / virtualSnapPointsY)) - bounds.y : 0;
                     Gizmos.DrawSphere(transform.TransformPoint(new Vector3(xOffset, yOffset, 0)), .0025f);
                 }
             }
