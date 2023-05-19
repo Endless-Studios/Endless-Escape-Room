7269e1807952a34de2bf0508f1c5df4fd1f18076
JoshEndless <jharris@endlessstudios.com>
2023-05-19 00:31:42 -0500

    Initial chasing implementation

---
+++ Assets/Scripts/Ai/AiIk.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Ai
+{
+    public class AiIk : MonoBehaviour
+    {
+        [SerializeField] private Animator animator;
+        public GameObject lookAtObject;
+        
+        private void OnAnimatorIK(int layerIndex)
+        {
+            if (animator)
+            {
+                animator.SetLookAtWeight(1);
+                animator.SetLookAtPosition(lookAtObject.transform.position);
+            }
+        }
+    }
+}
