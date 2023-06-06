16d0412bc19654ea9dad1eb8f9961a6401b9e6c9
olopezendless <mr.safacon@gmail.com>
2023-06-06 10:46:49 -0700

    Sound Manager's AudioSource pooling

--- Assets/Scripts/Sound/SoundManager.cs
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Sound
@@ -12,10 +14,55 @@ namespace Sound
         [field: SerializeField] public LayerMask SoundBlockerMask { get; private set; }
         public static event Action<EmittedSoundData> OnSoundEmitted;
 
+        [field: SerializeField] public AudioSource AudioSourcePrefab { get; private set; }
+
+        private List<AudioSource> audioSourcePool = new List<AudioSource>();
+
         public void EmitSoundAtPosition(EmittedSoundData soundData, AudioClip clip)
         {
             //PlayClip from pooled audio source
+            AudioSource useAudioSource = GetAvailableAudioSource();
+            useAudioSource.transform.position = soundData.Position;
+            useAudioSource.clip = clip;
+
+            //Schedule pooling
+            StartCoroutine(AddAudioSourceToPoolAfterDelay(useAudioSource, soundData.ClipLength));
+
             OnSoundEmitted?.Invoke(soundData);
+        }
+
+        private AudioSource GetAvailableAudioSource()
+        {
+            AudioSource useAudioSource = null;
+
+            //check pool for an available audio source
+            for (int i = 0; i < audioSourcePool.Count; i++)
+            {
+                if (audioSourcePool[i].isPlaying == false)
+                {
+                    useAudioSource = audioSourcePool[i];
+                    audioSourcePool.Remove(useAudioSource);
+                    break;
+                }
+            }
+
+            //no audio source available, create one
+            if (useAudioSource == null)
+                useAudioSource = CreateAudioSoruce();
+
+            return useAudioSource;
+        }
+
+        private AudioSource CreateAudioSoruce()
+        {
+            AudioSource createdAudioSource = GameObject.Instantiate<AudioSource>(AudioSourcePrefab);
+            return createdAudioSource;
+        }
+
+        IEnumerator AddAudioSourceToPoolAfterDelay(AudioSource targetAudioSource, float delaySeconds)
+        {
+            yield return new WaitForSeconds(delaySeconds);
+            audioSourcePool.Add(targetAudioSource);
         }
     }
 }
