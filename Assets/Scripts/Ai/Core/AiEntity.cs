c6848d6015a0f3135763983a33beade6811128fa
JoshEndless <jharris@endlessstudios.com>
2023-06-18 22:36:05 -0500

    Bug fixes, cleanup, and attack behavior adjustments.

--- Assets/Scripts/Ai/Core/AiEntity.cs
+++ Assets/Scripts/Ai/Core/AiEntity.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections;
-using UnityEngine;
 using UnityEngine.Events;
 
 // ReSharper disable UnusedMember.Global
--- Assets/Scripts/Ai/Core/GameplayInfo.cs
+++ Assets/Scripts/Ai/Core/GameplayInfo.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections;
-using Sight;
+﻿using System.Collections;
 using UnityEngine;
 
 namespace Ai
