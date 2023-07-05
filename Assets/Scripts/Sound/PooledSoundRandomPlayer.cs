fae70dcdc7157ff28d864a9bfae638faeb1bdf96
Safemilk <dwilliams@endlessstudios.com>
2023-07-05 16:18:32 -0700

    Keypad noises, door noises, doors and keypads now make noises. Duplicated the play pooled audio script and made a variant that just plays a random audio clip from the pool.

---
+++ Assets/Scripts/Sound/PooledSoundRandomPlayer.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Plays a sound clip using a pooled AudioSource via SoundPoolManager.
+/// Best used with shorter sound clips that dont loop. For longer/looping clips use an AudioSource in scene. 
+/// <summary>
+public class PooledSoundRandomPlayer : MonoBehaviour
+{
+    [SerializeField] private Transform soundPositionTransform;
+    [SerializeField] private AudioClip[] soundClips;
+
+    public UnityEvent<Vector3> OnSoundPlayedAtPosition = new UnityEvent<Vector3>();
+
+    private void OnValidate()
+    {
+        if(soundPositionTransform == null)
+            soundPositionTransform = transform;
+    }
+
+    public void PlaySound()
+    {
+        Sound.SoundPoolManager.Instance.PlaySoundAtPosition(soundPositionTransform.position, soundClips[Random.Range(0, soundClips.Length)]);
+        OnSoundPlayedAtPosition.Invoke(soundPositionTransform.position);
+    }
+}
